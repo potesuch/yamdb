57f82d65482568695693ad8bcbd7120a06c34099
potesuch <potesuch1@yandex.ru>
2024-04-09 22:17:54 +0300

    minor fixes

--- yamdb/yamdb/settings.py
+++ yamdb/yamdb/settings.py
@@ -28,7 +28,7 @@ load_dotenv(dotenv_path=env_path)
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tk5tsokrve(hy)myt8ob)7q)_-l2m&bitwfm$v9ytgy(3e$)wa'
+SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
