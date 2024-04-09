91eb11fb8774d1789e56c457424e7b97b2c0e27c
potesuch <potesuch1@yandex.ru>
2024-04-09 19:04:01 +0300

    minor fixes

--- yamdb/yamdb/test_settings.py
+++ yamdb/yamdb/test_settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-tk5tsokrve(hy)myt8ob)7q)_-l2m&bitwfm$v9ytgy(3e$)wa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.15', '127.0.0.1', 'testserver', 'localhost', 'web']
+ALLOWED_HOSTS = ['192.168.0.15', '127.0.0.1', 'testserver', 'localhost', 'webyamdb']
 
 
 # Application definition
