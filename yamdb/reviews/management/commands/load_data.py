9bba8dac2f221e920ae072baa25ee28b7ec70847
potesuch <potesuch1@yandex.ru>
2024-06-21 20:00:17 +0300

    add docs [skip ci]

--- yamdb/reviews/management/commands/load_data.py
+++ yamdb/reviews/management/commands/load_data.py
@@ -11,16 +11,38 @@ STATIC_ROOT = settings.STATIC_ROOT
 
 
 class Command(BaseCommand):
+    """
+    Класс команды Django для импорта данных из CSV файлов в соответствующие модели.
+    """
 
     def add_arguments(self, parser):
+        """
+        Добавляет аргументы командной строки для команды.
+
+        Args:
+            parser (argparse.ArgumentParser): Парсер аргументов.
+        """
         parser.add_argument('args', nargs='+', type=str)
 
     def handle(self, *args, **options):
+        """
+        Основной метод команды. Обрабатывает переданные аргументы и вызывает процесс импорта данных.
+
+        Args:
+            args (list): Список аргументов командной строки.
+            options (dict): Словарь дополнительных опций.
+        """
         csv_dict = self.get_csv_dict()
         for arg in args:
             self.process_csv_file(arg, csv_dict)
 
     def get_csv_dict(self):
+        """
+        Получает словарь соответствий моделей и CSV файлов.
+
+        Returns:
+            dict: Словарь, где ключи - имена моделей, значения - пути к CSV файлам.
+        """
         data_path = os.path.join(STATIC_ROOT, 'data')
         csv_files = glob(os.path.join(data_path, '*.csv'))
         csv_dict = {}
@@ -31,11 +53,27 @@ class Command(BaseCommand):
         return csv_dict
 
     def get_model_name(self, csv_name):
+        """
+        Преобразует имя CSV файла в имя модели.
+
+        Args:
+            csv_name (str): Имя CSV файла без расширения.
+
+        Returns:
+            str: Имя модели.
+        """
         if '_' in csv_name:
             return ''.join(p.capitalize() for p in csv_name.split('_'))
         return csv_name.capitalize()
 
     def process_csv_file(self, arg, csv_dict):
+        """
+        Обрабатывает CSV файл и импортирует данные в соответствующую модель.
+
+        Args:
+            arg (str): Имя модели.
+            csv_dict (dict): Словарь соответствий моделей и CSV файлов.
+        """
         csv_file = csv_dict.get(arg)
         if csv_file:
             with open(csv_file, 'r', encoding='utf-8') as f:
@@ -53,6 +91,13 @@ class Command(BaseCommand):
             ))
 
     def populate_model_data(self, model, reader):
+        """
+        Заполняет модель данными из CSV файла.
+
+        Args:
+            model (django.db.models.Model): Модель для заполнения.
+            reader (csv.DictReader): Объект для чтения CSV данных.
+        """
         mapping = {}
         data = {}
         fields = model._meta.get_fields()
@@ -70,6 +115,13 @@ class Command(BaseCommand):
             self.create_model_instance(model, data)
 
     def create_model_instance(self, model, data):
+        """
+        Создает экземпляр модели с данными и сохраняет его в базу данных.
+
+        Args:
+            model (django.db.models.Model): Модель для заполнения.
+            data (dict): Данные для заполнения модели.
+        """
         obj = model(**data)
         obj.save()
         self.stdout.write(self.style.NOTICE(
